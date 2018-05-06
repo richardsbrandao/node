26b63e78b8a6543bf53f7295c84d9aff3d917831
Richard Brandao <richardsbrandao@gmail.com>
2018-05-05 22:00:43 -0300

    Enable cors and fixing cache's key bug

--- express/mysqll/app/controllers/fights_controller.js
+++ express/mysqll/app/controllers/fights_controller.js
@@ -4,11 +4,13 @@ import cacheManager from "../../config/cache_manager"
 let controller = {}
 
 async function findFightsUsingCache(params) {
-    const key = params.enemyId + "_" + params.heroId
+    const key = (params.enemyId || '') + '_' + (params.heroId || '')
     const cache = await cacheManager.get(key)
-    if(cache != null) {
+    console.log('Cache return for keys: ', key)
+    if(cache) {
         return cache
     }
+    
     const value = await Fight.findBy({enemy_id: params.enemyId, hero_id: params.heroId})
     cacheManager.set(key, JSON.stringify(value))
     return value
--- express/mysqll/index.js
+++ express/mysqll/index.js
@@ -3,10 +3,12 @@ import bodyParser from "body-parser"
 import config from "./config/config.js"
 import router from "./config/router.js"
 import database from "./config/database.js"
+import cors from "cors"
 
 const app = express()
 database.connect(config['database'])
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(`${config['contextRoot']}`, router(app))
